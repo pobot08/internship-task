e3bc2042d96007479723a47d5c45195d05d4e97a
pobot08 <yii04042007@gmail.com>
2026-05-26 21:00:14 +0700

    Добавил папку Data с AppDbContext

---
+++ Service1.Generator/Data/AppDbContext.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore;
+using service1.Models;
+
+namespace service1.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+        public DbSet<Batch> Batches { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Batch>(entity =>
+            {
+                entity.HasKey(b => b.BatchId);
+
+                entity.Property(b => b.Items)
+                      .HasColumnType("jsonb")
+                      .HasConversion(
+                          v => System.Text.Json.JsonSerializer.Serialize(v, (System.Text.Json.JsonSerializerOptions?)null),
+                          v => System.Text.Json.JsonSerializer.Deserialize<List<DataItem>>(v, (System.Text.Json.JsonSerializerOptions?)null)!
+                      );
+            });
+        }
+    }
+}
